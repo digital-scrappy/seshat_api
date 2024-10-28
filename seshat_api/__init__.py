14fed67fb1a70295b2e9a2daf2364770d58cfba7
Ed Chalstrey <edwardchalstrey@gmail.com>
2024-10-28 16:46:58 +0000

    add Ed to init

--- seshat_api/__init__.py
+++ seshat_api/__init__.py
@@ -1,6 +1,6 @@
 from .seshat_api import SeshatAPI, get_frequencies, get_variable_classes
 
 __version__ = "0.1.1"
-__author__ = "Kalle Westerling"
-__email__ = "kwesterling@turing.ac.uk"
+__author__ = "Kalle Westerling, Ed Chalstrey"
+__email__ = "kwesterling@turing.ac.uk, echalstrey@turing.ac.uk"
 __all__ = ["SeshatAPI"]
